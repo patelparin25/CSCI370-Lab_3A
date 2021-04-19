ee4f44703d36367a5127edc27783de0caf2f0e8d
Parin Patel <parinpatel@Parins-MacBook-Pro.local>
2021-04-19 19:46:53 -0400

    Final Update

--- app/src/main/java/com/example/lab_3a_parinp/MainActivity.java
+++ app/src/main/java/com/example/lab_3a_parinp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.lab_3a_parinp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.ArrayList;
+import android.widget.ListView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<Student> studentArrayList = new ArrayList<>();
+        Student studentOne = new Student("Michael","Smith","Computer Science");
+        Student studentTwo = new Student("Valerie","Walker","Computer Science");
+        Student studentThree = new Student("Stephanie","Freeman","Computer Science");
+        Student studentFour = new Student("Stephen","Getbetter","Computer Science");
+        Student studentFive = new Student("Dwayne","Johnson","Computer Science");
+
+        studentArrayList.add(studentOne);
+        studentArrayList.add(studentTwo);
+        studentArrayList.add(studentThree);
+        studentArrayList.add(studentFour);
+        studentArrayList.add(studentFive);
+        StudentAdapter studentAdapter = new StudentAdapter(getApplicationContext(), studentArrayList);
+        ListView listView = findViewById(R.id.student);
+        listView.setAdapter(studentAdapter);
     }
 }
--- app/src/main/java/com/example/lab_3a_parinp/Student.java
+++ app/src/main/java/com/example/lab_3a_parinp/Student.java
@@ -1,4 +1,32 @@
 package com.example.lab_3a_parinp;
 
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String major;
+
+    public Student(String firstName, String lastName, String major) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.major = major;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public void setMajor(String major) {
+        this.major = major;
+    }
 }
